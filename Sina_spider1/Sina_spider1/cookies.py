70a83817710cc695b374cebaf0ba048b091eb9fa
beautiful523 <546139486@qq.com>
2018-02-05 14:39:14 +0800

    动态加入的scrawl_ID不再被爬

--- Sina_spider1/Sina_spider1/cookies.py
+++ Sina_spider1/Sina_spider1/cookies.py
@@ -7,12 +7,12 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import logging
-from yumdama import identify
+import json
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 IDENTIFY = 1  # 验证码输入方式:        1:看截图aa.png，手动输入     2:云打码
-COOKIE_GETWAY = 0 # 0 代表从https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.18) 获取cookie   # 1 代表从https://weibo.cn/login/获取Cookie
+COOKIE_GETWAY = 1  # 0 代表从https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.18) 获取cookie   # 1 代表从https://weibo.cn/login/获取Cookie
 dcap = dict(DesiredCapabilities.PHANTOMJS)  # PhantomJS需要使用老版手机的user-agent，不然验证码会无法通过
 dcap["phantomjs.page.settings.userAgent"] = (
     "Mozilla/5.0 (Linux; U; Android 2.3.6; en-us; Nexus S Build/GRK39F) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"
@@ -27,9 +27,10 @@ logging.getLogger("selenium").setLevel(logging.WARNING)  # 将selenium的日志�
 或者你也可以把时间间隔调大点。
 """
 myWeiBo = [
-    {'no': 'jiadieyuso3319@163.com', 'psw': 'a123456'},
-    {'no': 'shudieful3618@163.com', 'psw': 'a123456'},
+    {'no': 'm13611044648@163.com', 'psw': 'zhouyawen523'},
+    {'no': '18610379194', 'psw': 'tuyue7208562'},
 ]
+
 
 def getCookie(account, password):
     if COOKIE_GETWAY == 0:
@@ -38,6 +39,7 @@ def getCookie(account, password):
         return get_cookie_from_weibo_cn(account, password)
     else:
         logger.error("COOKIE_GETWAY Error!")
+
 
 def get_cookie_from_login_sina_com_cn(account, password):
     """ 获取一个账号的Cookie """
@@ -76,62 +78,62 @@ def get_cookie_from_login_sina_com_cn(account, password):
 
 def get_cookie_from_weibo_cn(account, password):
     """ 获取一个账号的Cookie """
-    try:
-        browser = webdriver.PhantomJS(desired_capabilities=dcap)
-        browser.get("https://weibo.cn/login/")
-        time.sleep(1)
+    # try:
+    browser = webdriver.PhantomJS(desired_capabilities=dcap)
+    browser.get("https://weibo.cn/login/")
+    time.sleep(1)
 
-        failure = 0
-        while "微博" in browser.title and failure < 5:
-            failure += 1
-            browser.save_screenshot("aa.png")
-            username = browser.find_element_by_name("mobile")
-            username.clear()
-            username.send_keys(account)
+    failure = 0
+    while "微博" in browser.title and failure < 5:
+        failure += 1
+        browser.save_screenshot("aa.png")
+        username = browser.find_element_by_id("loginName")
+        username.clear()
+        username.send_keys(account)
 
-            psd = browser.find_element_by_xpath('//input[@type="password"]')
-            psd.clear()
-            psd.send_keys(password)
-            try:
-                code = browser.find_element_by_name("code")
-                code.clear()
-                if IDENTIFY == 1:
-                    code_txt = raw_input("请查看路径下新生成的aa.png，然后输入验证码:")  # 手动输入验证码
-                else:
-                    from PIL import Image
-                    img = browser.find_element_by_xpath('//form[@method="post"]/div/img[@alt="请打开图片显示"]')
-                    x = img.location["x"]
-                    y = img.location["y"]
-                    im = Image.open("aa.png")
-                    im.crop((x, y, 100 + x, y + 22)).save("ab.png")  # 剪切出验证码
-                    code_txt = identify()  # 验证码打码平台识别
-                code.send_keys(code_txt)
-            except Exception, e:
-                pass
-
-            commit = browser.find_element_by_name("submit")
-            commit.click()
-            time.sleep(3)
-            if "我的首页" not in browser.title:
-                time.sleep(4)
-            if '未激活微博' in browser.page_source:
-                print '账号未开通微博'
-                return {}
-
-        cookie = {}
-        if "我的首页" in browser.title:
-            for elem in browser.get_cookies():
-                cookie[elem["name"]] = elem["value"]
-            logger.warning("Get Cookie Success!( Account:%s )" % account)
-        return json.dumps(cookie)
-    except Exception, e:
-        logger.warning("Failed %s!" % account)
-        return ""
-    finally:
+        psd = browser.find_element_by_xpath('//input[@type="password"]')
+        psd.clear()
+        psd.send_keys(password)
         try:
-            browser.quit()
+            code = browser.find_element_by_name("code")
+            code.clear()
+            if IDENTIFY == 1:
+                code_txt = raw_input("请查看路径下新生成的aa.png，然后输入验证码:")  # 手动输入验证码
+        # else:
+        #     from PIL import Image
+        #     img = browser.find_element_by_xpath('//form[@method="post"]/div/img[@alt="请打开图片显示"]')
+        #     x = img.location["x"]
+        #     y = img.location["y"]
+        #     im = Image.open("aa.png")
+        #     im.crop((x, y, 100 + x, y + 22)).save("ab.png")  # 剪切出验证码
+        #     code_txt = identify()  # 验证码打码平台识别
+            code.send_keys(code_txt)
         except Exception, e:
             pass
+
+        commit = browser.find_element_by_id("loginAction")
+        commit.click()
+        time.sleep(3)
+        if "我的首页" not in browser.title:
+            time.sleep(4)
+        if '未激活微博' in browser.page_source:
+            print '账号未开通微博'
+            return {}
+
+    cookie = {}
+    if "我的首页" in browser.title:
+        for elem in browser.get_cookies():
+            cookie[elem["name"]] = elem["value"]
+        logger.warning("Get Cookie Success!( Account:%s )" % account)
+    return json.dumps(cookie)
+    # except Exception, e:
+    #     logger.warning("Failed %s!" % account)
+    #     return ""
+    # finally:
+    #     try:
+    browser.quit()
+    #     except Exception, e:
+    #         pass
 
 
 
